6e5055c252eaf6ab3664dcf4ba1d32912da0477b
ljliujun <469357434@qq.com>
2026-08-12 23:25:25 +0800

    11

--- LANDrop/discoveryservice.cpp
+++ LANDrop/discoveryservice.cpp
@@ -112,6 +112,7 @@ void DiscoveryService::socketReadyRead()
         if (isLocalAddress(addr))
             continue;
         
+        
 
         QJsonDocument json = QJsonDocument::fromJson(data);
         if (!json.isObject())
